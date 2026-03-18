8569d1f9a829e5f2b57244dec02e2c132cd59965
AliAhY1 <aliiiahali3@gmail.com>
2026-03-19 00:31:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -168,7 +168,7 @@ def main():
 
     app.run_webhook(
         listen="0.0.0.0",
-        port=int(os.getenv("PORT", 8080)),
+        port=int(os.environ.get("PORT"))
         url_path=TOKEN,
         webhook_url=f"{WEBHOOK_URL}/{TOKEN}",
     )
